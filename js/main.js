f64a4e5ab4910965247937694caa0215f9b41bee
phj <parkhj8801@gmail.com>
2019-06-18 01:44:43 +0900

    add Youtube auto play

--- js/main.js
+++ js/main.js
@@ -32,6 +32,12 @@ $(document).ready(function(){
     //상단 메뉴 고정(stickyMenu)
     var $header = $('header');
     var $services = $('.services');
+    var $couters = $('.counters');
+    var $counterData = $couters.find('h3');
+    var $serciveExecuted = false;
+    var $couterExecuted = false;
+    
+    
     $(window).scroll(function(){
 
         //현재의 스크롤 양
@@ -46,17 +52,68 @@ $(document).ready(function(){
 
         //service-item 나타나기
         var $serviceThreshold = $services.offset().top - $offset;
-        var $serciveExecuted = false;
+        
         if(!$serciveExecuted){  
             if($currentSct > $serviceThreshold){
                 $services.addClass('active');
                 $serciveExecuted = true;
             }
         }
+        
+        //숫자 애니메이션
+        var $couterThreshold = $couters.offset().top - $offset; 
+        
+        if(!$couterExecuted){  
+            if($currentSct > $couterThreshold){
+                $counterData.each(function(){
+                    var $current = $(this);
+                    var $target = $current.attr('data-rate');
+                    
+                    //animate, progress, rate
+                    $({rate:0}).animate({rate:$target},{
+                        duration:2500,
+                        progress:function(){
+                            var now = this.rate;
+                            $current.text(Math.ceil(now));
+                        }
+                    });
+                    
+                });
+                $couterExecuted = true;
+            } //$counterExecuted if
+            
+        } //scroll if  
     }); //scroll event    
+    
+    
+    //overlay
+    var currentUrl = $('iframe').attr('src');
+    $('.video .icon').click(function(e){
+        e.preventDefault();
+        $('#overlay').addClass('visible');
+        
+        var newStr = '?autoplay=1';
+        var newUrl = currentUrl.concat(newStr);
+        
+        $('iframe').attr('src',newUrl);
+        
+    });
+    $('.video .close').click(function(e){
+        e.preventDefault();
+        var newUrl2 = currentUrl.replace('?autoplay=1','');
+        $('iframe').attr('src',newUrl2);
+        
+        $('#overlay').removeClass('visible');
+    });
+    
+    
+    
+    
+    
+    
     
   
     
     
     
-    });
+});
